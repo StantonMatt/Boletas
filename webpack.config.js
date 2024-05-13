29cc31f19eb3481bbf4ee83dcb1894cbfffddb12
DonMatiu <98232499+DonMatiu@users.noreply.github.com>
2024-05-13 12:43:06 -0400

    adding .github workflows

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,69 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    bundle: path.resolve(__dirname, 'src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
-    clean: true,
-    assetModuleFilename: '[name][ext]',
-  },
-  devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: {
+      bundle: path.resolve(__dirname, 'src/index.js'),
     },
-    port: 5000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-    watchFiles: ['./src/*'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name][contenthash].js',
+      clean: true,
+      assetModuleFilename: '[name][ext]',
+    },
+    devtool: isProduction ? 'none' : 'source-map',
+    devServer: isProduction
+      ? undefined
+      : {
+          static: {
+            directory: path.resolve(__dirname, 'dist'),
+          },
+          port: 5000,
+          open: true,
+          hot: true,
+          compress: true,
+          historyApiFallback: true,
+          watchFiles: ['./src/*'],
+        },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|bmp|pdf)$/i,
-        type: 'asset/resource',
-      },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|bmp|pdf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'COAB',
+        filename: 'index.html',
+        template: 'src/indexTemplate.html',
+      }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'COAB',
-      filename: 'index.html',
-      template: 'src/indexTemplate.html',
-    }),
-  ],
+    optimization: {
+      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
+  };
 };
